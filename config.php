c78f6d68b65997d3a26e0286a3a7421eae8f25be
Martin Frericks <mfrericks@gmail.com>
2016-08-21 15:16:16 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "firebase");
+define("DB_PASSWORD", "dGaKidEd");
+define("DB_NAME", "firebase");
+
+define("SERVER_KEY", "AIzaSyBXkUIe3Mcg_jgXgFJsyvqyJ5LtIUNER8w");
